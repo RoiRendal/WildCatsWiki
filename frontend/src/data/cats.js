9686c76fcc4f48dcac1164dd039eca10811f905a
RoiRendal <23-16160@g.batstate-u.edu.ph>
2025-12-10 09:23:16 +0800

    chore: adding sample big cats data for database

---
+++ frontend/src/data/cats.js
@@ -0,0 +1,50 @@
+export const cats = [
+  {
+    id: "tiger",
+    name: "Bengal Tiger",
+    scientificName: "Panthera tigris tigris",
+    description: "The Bengal tiger is a population of the Panthera tigris tigris subspecies. It ranks among the biggest wild cats alive today.",
+    image: "",
+    stats: {
+      lifespan: "10-15 years",
+      speed: "56 km/h",
+      weight: "230 kg"
+    }
+  },
+  {
+    id: "lion",
+    name: "African Lion",
+    scientificName: "Panthera leo",
+    description: "The lion is a large cat of the genus Panthera native to Africa and India. It uses a cooperative hunting strategy.",
+    image: "",
+    stats: {
+      lifespan: "10-14 years",
+      speed: "74 km/h",
+      weight: "206 kg"
+    }
+  },
+  {
+    id: "leopard",
+    name: "Leopard",
+    scientificName: "Panthera pardus",
+    description: "The leopard is one of the five extant species in the genus Panthera. It is known for its well-camouflaged fur and climbing ability.",
+    image: "",
+    stats: {
+      lifespan: "12-17 years",
+      speed: "58 km/h",
+      weight: "51 kg"
+    }
+  },
+  {
+    id: "jaguar",
+    name: "Jaguar",
+    scientificName: "Panthera onca",
+    description: "The jaguar is a large cat species and the only living member of the genus Panthera native to the Americas.",
+    image: "",
+    stats: {
+      lifespan: "12-15 years",
+      speed: "80 km/h",
+      weight: "96 kg"
+    }
+  }
+];
