58393671c1c8d8a655d80b0789ab5dd8402ede55
RoiRendal <23-16160@g.batstate-u.edu.ph>
2025-12-11 08:29:23 +0800

    feat: add a base quiz page

---
+++ frontend/src/pages/Quiz.jsx
@@ -0,0 +1,11 @@
+import {questions} from '../data/questions' ;
+import { Link } from 'react-router-dom';
+import { useState } from 'react';
+
+function Quiz() {
+    return (
+        <div></div>
+    )
+}
+
+export default Quiz;
